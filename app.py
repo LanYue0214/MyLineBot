9658d8e352861301b783814a251eb22d0ebbd07d
LanYue0214 <49686760+LanYue0214@users.noreply.github.com>
2021-06-22 22:05:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -65,16 +65,9 @@ def handle_message(event):
         )
             line_bot_api.reply_message(event.reply_token, buttons_template_message)
     elif re.match('蘇媽今天發布了什麼',message):
-        video_message = VideoSendMessage(
-            original_content_url='https://youtu.be/xtrhHH0kQI0',
-            preview_image_url='https://i.imgur.com/yebDeyq.png'
-        )
-        line_bot_api.reply_message(event.reply_token, video_message)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='https://youtu.be/xtrhHH0kQI0'))
     elif re.match('test',message):
-        message = TextSendMessage(
-            text = 'cry\ncry2'
-        )
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text='cry\ncry2'))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=message))
 
